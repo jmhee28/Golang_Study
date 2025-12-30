d53194f01dc0773e033576f6f40d8a9079b72bb2
Joung MyeongHee <jmhee3411@gmail.com>
2025-12-30 23:22:57 +0900

    closure

---
+++ day1/function/closure.go
@@ -0,0 +1,21 @@
+package main
+ 
+func nextValue() func() int { // int를 리턴하는 익명함수(func() int)를 리턴하는 함수
+    i := 0
+    return func() int {
+        i++
+        return i
+    }
+}
+ 
+func main() {
+    next := nextValue()
+ 
+    println(next())  // 1
+    println(next())  // 2
+    println(next())  // 3
+ 
+    anotherNext := nextValue()
+    println(anotherNext()) // 1 다시 시작
+    println(anotherNext()) // 2
+}
